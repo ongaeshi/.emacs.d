fbb2bca29af34f951ed2570c17ff42af3cb868d5
ongaeshi <ongaeshi0621@gmail.com>
2015-12-20 17:32:10 +0900

    backup

---
+++ elpa/jsx-mode-20130908.1024/jsx-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; jsx-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (jsx-mode) "jsx-mode" "jsx-mode.el" (21820 42091
+;;;;;;  0 0))
+;;; Generated autoloads from jsx-mode.el
+
+(autoload 'jsx-mode "jsx-mode" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("jsx-mode-pkg.el") (21820 42091 176024
+;;;;;;  0))
+
+;;;***
+
+(provide 'jsx-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jsx-mode-autoloads.el ends here
