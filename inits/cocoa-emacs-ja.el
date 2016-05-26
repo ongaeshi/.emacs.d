fc971fc7a5769e75c8e1ffabd5ced768d18c6b53
ongaeshi <ongaeshi0621@gmail.com>
2016-05-27 00:15:31 +0900

    Change default font

--- inits/cocoa-emacs-ja.el
+++ inits/cocoa-emacs-ja.el
@@ -1,20 +1,20 @@
 ;; 英語フォント(Monaco)
-;; (set-face-attribute 'default nil
-;;                     :family "Monaco"
-;;                     :height 140)
+(set-face-attribute 'default nil
+                    :family "Monaco"
+                    :height 140)
 
 ;; 日本語フォント(ヒラギノ角ゴProN)
-;; (set-fontset-font "fontset-default"
-;;                   'japanese-jisx0208
-;;                   '("Hiragino Maru Gothic ProN"))
-
-(set-face-attribute 'default nil
-                    :family "Source Han Code JP"
-                    :height 120)
-
 (set-fontset-font "fontset-default"
                   'japanese-jisx0208
-                  '("Source Han Code JP"))
+                  '("Hiragino Maru Gothic ProN"))
+
+;; (set-face-attribute 'default nil
+;;                     :family "Source Han Code JP"
+;;                     :height 120)
+
+;; (set-fontset-font "fontset-default"
+;;                   'japanese-jisx0208
+;;                   '("Source Han Code JP"))
 
 ;; Yen -> Backslash
 (define-key global-map [?\¥] [?\\])
